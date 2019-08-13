9f422fd52029e8248f4e4d88bd9f3a3c4892e6b7
henriquesd <hsd.foz@gmail.com>
2019-08-13 20:14:57 -0300

    Autenticação

--- AspNetCoreIdentity/Controllers/HomeController.cs
+++ AspNetCoreIdentity/Controllers/HomeController.cs
@@ -1,20 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using AspNetCoreIdentity.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace AspNetCoreIdentity.Controllers
 {
+    // O "Authorize" implementa a necessidade do usuário estar autenticado na aplicação.
+    // (Autenticação significa o usuário ter feito login e ter sido autorizado pela aplicação)
+    // Pode por em cada um dos métodos da Controller, ou mover para toda a Controller.
+    // Para abrir exceções (que podem ser acessados por anônimos), utilize o "AllowAnonymous";
+    [Authorize]
     public class HomeController : Controller
     {
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
         }
 
+        //[Authorize]
         public IActionResult Privacy()
         {
             return View();
